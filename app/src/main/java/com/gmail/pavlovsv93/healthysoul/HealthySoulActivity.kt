d605d646a229b6722c320b9624b1a789f46d0698
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-10-18 13:22:47 +0300

    Начало проекта

---
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/HealthySoulActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.pavlovsv93.healthysoul
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HealthySoulActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
