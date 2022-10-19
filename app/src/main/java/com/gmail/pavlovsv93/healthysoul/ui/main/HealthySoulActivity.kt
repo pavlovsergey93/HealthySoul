f2f474ee1f207551c117f8abaacc618b4c1ae3c6
Nikita Bogatov <bogatovnikita@gmail.com>
2022-10-19 22:32:31 +0300

    add modules data and domain. Add min architecture

--- app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/main/HealthySoulActivity.kt
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/main/HealthySoulActivity.kt
@@ -1,7 +1,8 @@
-package com.gmail.pavlovsv93.healthysoul
+package com.gmail.pavlovsv93.healthysoul.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gmail.pavlovsv93.healthysoul.R
 
 class HealthySoulActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
