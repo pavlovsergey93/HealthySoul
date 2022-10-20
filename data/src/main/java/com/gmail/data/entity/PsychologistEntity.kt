4f763d97e24456a04f14e2a449d834cd7e61e831
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-10-20 15:27:45 +0300

    Add entity psychologist in the data module

---
+++ data/src/main/java/com/gmail/data/entity/PsychologistEntity.kt
@@ -0,0 +1,37 @@
+package com.gmail.data.entity
+
+data class PsychologistEntity(
+	val id: String,
+	val avatar: String,                    // Аватарка
+	val name: String,                        // Имя
+	val surname: String,                    // Фамилия
+	val patronymic: String,                // Отчество
+	val profile: String,                    // Информация о том, чем ты можешь помочь человеку
+	val country: String,                    // Страна
+	val city: String,                        // Город
+	val contact: List<Contact>,                // Контакты
+	val education: List<Education>,            // Образование
+	val specialization: Specialization,        //Специализация
+	val experience: Int,                       // Опыт работы в количестве лет
+	val rating: Double,                        // Рейтинг
+	val numberOfVotes: Int						// Количество голосов
+) {
+
+	data class Contact(
+		val titleContact: String,            // Телефон, почта и т.д.
+		val contact: String                    // Сам контакт
+	)
+
+	data class Education(
+		val university: String,                // Учреждение образования
+		val faculty: String,                // Факультет
+		val specialization: String,            // Специальность
+		val yearOfGraduation: Int            // Год окончания
+	)
+
+	data class Specialization(
+		val profession: String,                // Специальность
+		val specialization: List<String>,    // Специализация
+		val direction: String                // Направление деятельности
+	)
+}
