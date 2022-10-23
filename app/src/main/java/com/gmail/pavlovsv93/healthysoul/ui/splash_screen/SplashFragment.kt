42956b55fd1334e86c188f23bf1f20dd4765effd
Angelinkaa <angelinamscw@gmail.com>
2022-10-23 23:23:17 +0300

    Add splash screen and greeting screen

--- app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/splash_screen/SplashFragment.kt
@@ -1,6 +1,88 @@
-package com.gmail.pavlovsv93.healthysoul.ui.splash_screen
+package com.gmail.pavlovsv93.healthysoul.ui.splash
 
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.gmail.pavlovsv93.healthysoul.R
+
 
 class SplashFragment : Fragment() {
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "onCreateView")
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated")
+        Handler(Looper.getMainLooper()).postDelayed(object : Runnable {
+            override fun run() {
+                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToGreetingFragment())
+            }
+        }, 3000)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "onDetach")
+    }
+
+    companion object {
+
+        private const val TAG = "SplashFragment"
+
+    }
+
 }
