5b8e00de5f4cd8ab0d6ab91ffec0d1df56f058d9
79218 <kangerts9@gmail.com>
2022-11-14 13:34:30 +0300

    profile

---
+++ data/src/main/java/com/gmail/data/entity/Profile.kt
@@ -0,0 +1,9 @@
+package com.gmail.data.entity
+
+
+class Profile (
+    val avatar: String,                  // Аватарка
+    val name: String,                    // Имя
+    val number: Int,                     // Мобильный телефон
+    val type: String                     //Тип личности
+)
