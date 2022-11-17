e4a9dec2c5caf8a42466c5442dbb7570ba67f0c5
Alexander Voronov <dr.strum@gmail.com>
2022-11-17 21:29:10 +0300

    Last commit

--- app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/psychologist_screen/PsychologistFragment.kt
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/psychologist_screen/PsychologistFragment.kt
@@ -5,13 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.gmail.data.repository.FirebaseRepository
 import com.gmail.pavlovsv93.healthysoul.R
 import com.gmail.pavlovsv93.healthysoul.databinding.FragmentPsychologistBinding
 
 class PsychologistFragment : Fragment(R.layout.fragment_psychologist) {
 
+    private val viewModel: PsychologistViewModel by viewModels {
+        PsychologistViewModelFactory(FirebaseRepository())
+    }
+
     private var _binding: FragmentPsychologistBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null){
+            viewModel.requestAllPsychologist()
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,5 +41,4 @@ class PsychologistFragment : Fragment(R.layout.fragment_psychologist) {
         super.onDestroy()
         _binding = null
     }
-
 }
--- app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/psychologist_screen/PsychologistViewModel.kt
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/psychologist_screen/PsychologistViewModel.kt
@@ -1,4 +1,29 @@
 package com.gmail.pavlovsv93.healthysoul.ui.psychologist_screen
 
-class PsychologistViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.gmail.data.repository.FirebaseRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+class PsychologistViewModel(val repository: FirebaseRepository) : ViewModel() {
+
+    fun requestAllPsychologist() {
+
+        viewModelScope.launch {
+            try {
+                repository.getAllData()
+            } catch (_: IOException) {
+            }
+
+        }
+    }
+}
+
+class PsychologistViewModelFactory(private val repository: FirebaseRepository) :
+    ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+        PsychologistViewModel(repository) as T
 }
