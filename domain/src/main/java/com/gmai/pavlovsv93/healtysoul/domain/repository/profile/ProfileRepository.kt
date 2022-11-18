1eb9236c4d6f359d88dd911ef8567ffd49afd660
79218 <kangerts9@gmail.com>
2022-11-18 21:02:32 +0300

    profile

---
+++ domain/src/main/java/com/gmai/pavlovsv93/healtysoul/domain/repository/profile/ProfileRepository.kt
@@ -0,0 +1,8 @@
+package com.gmai.pavlovsv93.healtysoul.domain.repository.profile
+
+import com.gmai.pavlovsv93.healtysoul.domain.models.profile.Profile
+
+
+interface ProfileRepository  {
+    suspend fun getAllNotes(): Profile
+}
