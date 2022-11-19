55ea8055797babcc27cc2cbe7d54f78bef2101f6
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-11-19 14:33:50 +0300

    Continuing work on Questions

--- data/src/main/java/com/gmail/data/entity/tests/questionentity/QuestionEntity.kt
+++ data/src/main/java/com/gmail/data/entity/tests/questionentity/QuestionEntity.kt
@@ -3,7 +3,7 @@ package com.gmail.data.entity.tests.questionentity
 data class QuestionEntity(
 	val questionId: String,
 	val question: String,
-	val answers: List<Answer>
+	val answers: List<Answer>?
 ) {
 	data class Answer(
 		val answer: String,
