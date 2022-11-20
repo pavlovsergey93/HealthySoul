e448acc88e454a96f4854892099967e26114b203
Angelina <angelinamscw@gmail.com>
2022-11-20 20:38:58 +0300

    Change SplashFragment.kt

--- app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/splash_screen/SplashFragment.kt
@@ -21,12 +21,6 @@ class SplashFragment : Fragment() {
     private val viewModel by viewModels<LoginViewModel>()
     private lateinit var binding: FragmentSplashBinding
 
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        Log.d(TAG, "onAttach")
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentSplashBinding.inflate(layoutInflater)
@@ -64,45 +58,6 @@ class SplashFragment : Fragment() {
                 }
             }
         },3000)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Log.d(TAG, "onDestroyView")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        Log.d(TAG, "onDetach")
-    }
-
-    companion object {
-        private const val TAG = "SplashFragment"
     }
 }
 
