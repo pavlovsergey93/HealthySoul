59d98f856fe18c8696104e5beaf128ae00230d78
Angelina <angelinamscw@gmail.com>
2022-11-20 18:22:57 +0300

    Change

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -52,7 +52,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.6.1")
 	implementation("com.google.firebase:firebase-firestore:24.4.0")
-	testImplementation("junit:junit:4.13.2")
+    implementation("com.google.firebase:firebase-auth-ktx:21.1.0")
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
