56c7ce7c5371aaec2ce1e06f3a649688bdd8f8af
Angelina <angelinamscw@gmail.com>
2022-11-20 18:04:27 +0300

    Change greeting

--- app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/splash_screen/SplashFragment.kt
@@ -65,5 +65,44 @@ class SplashFragment : Fragment() {
             }
         },3000)
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "onDetach")
+    }
+
+    companion object {
+        private const val TAG = "SplashFragment"
+    }
 }
 
