8462295a2daad0ac83cb5b2097bc31585155ae56
Angelinkaa <angelinamscw@gmail.com>
2022-11-27 15:29:00 +0300

    Add files via upload

--- app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/splash_screen/SplashFragment.kt
@@ -1,9 +1,7 @@
 package com.gmail.pavlovsv93.healthysoul.ui.splash
 
-import android.content.Context
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.service.controls.ControlsProviderService.TAG
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,14 +10,13 @@ import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.gmail.pavlovsv93.healthysoul.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class SplashFragment : Fragment() {
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        Log.d(TAG, "onAttach")
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,62 +28,20 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         Log.d(TAG, "onCreateView")
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d(TAG, "onViewCreated")
-		view.findViewById<ImageView>(R.id.iv_logotype).animate()
-			.rotationBy(720f).setDuration(4000L).start()
-        Handler(Looper.getMainLooper()).postDelayed(object : Runnable {
-            override fun run() {
-//				findNavController().navigate(SplashFragmentDirections.actionGreetingFragmentToTestsFragment())
+        view.findViewById<ImageView>(R.id.iv_logotype).animate()
+            .rotationBy(720f).setDuration(4000L).start()
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(3000)
+            CoroutineScope(Dispatchers.Main).launch {
                 findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToGreetingFragment())
             }
-        }, 3000)
+        }
     }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Log.d(TAG, "onDestroyView")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        Log.d(TAG, "onDetach")
-    }
-
-    companion object {
-
-        private const val TAG = "SplashFragment"
-
-    }
-
 }
+
