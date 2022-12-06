672349714849323c1d6968de1aca6c54c2c75b71
kange <kangerts9@gmail.com>
2022-12-06 17:40:15 +0300

    profile

--- app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/profile/ProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.gmail.pavlovsv93.healthysoul.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment() {
 
@@ -19,6 +20,21 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeAuthenticationState()
+    }
+
+    private fun observeAuthenticationState() {
+        val name = String.format(FirebaseAuth.getInstance().currentUser?.displayName?:"Дарья")
+        val number = String.format(FirebaseAuth.getInstance().currentUser?.phoneNumber?:"2-51-97")
+        val email = String.format(FirebaseAuth.getInstance().currentUser?.email?:"kangert")
+        binding.nameText.setText(name)
+        binding.phoneNumber.setText(number)
+        binding.emailText.setText(email)
+
     }
 
     override fun onDestroy() {
