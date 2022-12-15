7143ef99dab8a0386ff0c2a22d17a6569b7be0a2
Angelina <angelinamscw@gmail.com>
2022-12-15 16:05:24 +0300

    Add FavoritesViewModel and progress bar

--- app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/favorites/FavoritesViewModel.kt
@@ -2,38 +2,33 @@ package com.gmail.pavlovsv93.healthysoul.ui.favorites
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.gmail.data.data.favorites.FavoritesDataSourceInterface
-import com.gmail.data.data.psychologist.PsychologistDataSourceInterface
-import com.gmail.data.data.room.RoomEntity
-import com.gmail.data.entity.PsychologistEntity
+import com.gmail.data.data.psychologist.DetailsPsychologistDataSourceInterface
 import com.gmail.pavlovsv93.healthysoul.utils.AppState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FavoritesViewModel(
-    private val dataSource: FavoritesDataSourceInterface,
+    private val dataSource: DetailsPsychologistDataSourceInterface,
     private val stateFlow: MutableStateFlow<AppState> = MutableStateFlow(AppState.OnLoading)
-): ViewModel() {
+) : ViewModel() {
+    fun getData(): StateFlow<AppState> = stateFlow.asStateFlow()
 
-    fun getData() = stateFlow.asStateFlow()
-
-    fun getAllFavorites() = viewModelScope.launch {
-        stateFlow.value = AppState.OnLoading
-        dataSource.getAllFavorites()
-            .catch { exc ->
-                stateFlow.value = AppState.OnException(exc)
+    fun getAllFavorites() = viewModelScope.launch(Dispatchers.IO) {
+        stateFlow.tryEmit(AppState.OnLoading)
+        dataSource.getDetailsPsychologist(id)
+            .catch { exp ->
+                withContext(Dispatchers.Main){
+                    stateFlow.tryEmit(AppState.OnException(exp))
+                }
+            }.collect { psychologist ->
+                withContext(Dispatchers.Main){
+                    stateFlow.tryEmit(AppState.OnSuccess(psychologist))
+                }
             }
-            .collect { data ->
-                stateFlow.value = AppState.OnSuccess(data)
-            }
-    }
-
-    fun deleteFavorite(psychologist: RoomEntity) = viewModelScope.launch {
-        dataSource.deleteFavorite(psychologist)
-    }
-    fun addFavorite(psychologist: RoomEntity) = viewModelScope.launch {
-        dataSource.addFavorite(psychologist)
     }
 }
