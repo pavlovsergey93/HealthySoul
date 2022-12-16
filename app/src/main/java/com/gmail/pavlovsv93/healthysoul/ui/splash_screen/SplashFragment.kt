c9034635a016460e646849a2b6a17a9c533be7c4
Angelina <angelinamscw@gmail.com>
2022-12-16 23:55:34 +0300

    Fixed SplashFragment.kt

--- app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/com/gmail/pavlovsv93/healthysoul/ui/splash_screen/SplashFragment.kt
@@ -1,8 +1,6 @@
 package com.gmail.pavlovsv93.healthysoul.ui.splash_screen
 
 import android.os.Bundle
-import android.service.controls.ControlsProviderService.TAG
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,22 +16,15 @@ import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate")
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d(TAG, "onCreateView")
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(TAG, "onViewCreated")
         view.findViewById<ImageView>(R.id.iv_logotype).animate()
             .rotationBy(720f).setDuration(4000L).start()
         CoroutineScope(Dispatchers.IO).launch {
